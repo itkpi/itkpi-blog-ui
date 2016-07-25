9d8266d2e972db0315f777eff505cc130effae66
Ihor Motornyi <IgorMotorny@users.noreply.github.com>
2016-07-25 22:59:46 +0300

    Ad component (#4)

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export Footer from './Footer/Footer';
 export IconLink from './IconLink/IconLink';
 export LeftSidebar from './LeftSidebar/LeftSidebar';
 export RightSidebar from './RightSidebar/RightSidebar';
+export Ad from './Ad/Ad';
